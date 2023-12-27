937ac329b4b3a30b150bcc563045e27dcbd88982
meedotakou <mohamed.otakoucht@edu.uiz.ac.ma>
2023-12-27 11:12:30 +0100

    trello app commit

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,26 @@
+import { Separator } from "@/components/ui/separator";
+import { Suspense } from "react";
+import { BoardList } from "./_components/board-list";
+import { Info } from "./_components/info";
+
+/*import { checkSubscription } from "@/lib/subscription"; */
+
+
+const OrganizationIdPage = () => {
+  /*     const boards = await db.board.findMany();
+   */  /* const isPro = await checkSubscription(); */
+  //const { organization, isLoaded } = useOrganization();
+  return (
+    <div className="w-full mb-20">
+      <Info isPro={false} />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <Suspense fallback={<BoardList.Skeleton />}  >
+          <BoardList  />
+        </Suspense>
+      </div>
+    </div>
+  );
+};
+
+export default OrganizationIdPage;
